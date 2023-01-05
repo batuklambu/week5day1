165c482711dafbc83e7d308fac458f99409ca178
batuklambu <batuklambu@gmail.com>
2023-01-05 17:29:38 -0500

    week5day1

--- index.js
+++ index.js
@@ -1,40 +1,31 @@
 // Menu data structure
 var menuLinks = [
-    {text: 'about', href: '/about'},
-    {text: 'catalog', href: '/catalog'},
-    {text: 'orders', href: '/orders'},
-    {text: 'account', href: '/account'},
-  ];
+  { text: 'about', href: '/about' },
+  { text: 'catalog', href: '/catalog' },
+  { text: 'orders', href: '/orders' },
+  { text: 'account', href: '/account' },
+];
 
 //Task 1
 
-
-let mainE1 = document.querySelector("main");
-mainE1.style.backgroundColor = "var(--main-bg)";
-mainE1.innerHTML = "<h1>SEI Rocks!</h1>";
+let mainE1 = document.querySelector('main');
+mainE1.style.backgroundColor = 'var(--main-bg)';
+mainE1.innerHTML = '<h1>SEI Rocks!</h1>';
 mainE1.classList.add('flex-ctr');
-
-
-
 
 // Task 2
 
-let topMenuE1 = document.getElementById("top-menu");  
-topMenuE1.style.backgroundColor = "var(--top-menu-bg)"; 
-topMenuE1.style.height='100%';
+let topMenuE1 = document.getElementById('top-menu');
+topMenuE1.style.backgroundColor = 'var(--top-menu-bg)';
+topMenuE1.style.height = '100%';
 topMenuE1.classList.add('flex-around');
-
 
 // Task 3
 
-menuLinks.forEach(function(link){
-    // console.log(link);
-let a = document.createElement('a');
-a.setAttribute('href','_link');
-a.textContent= link.text
-topMenuE1.appendChild(a);
-
+menuLinks.forEach(function (link) {
+  // console.log(link);
+  let a = document.createElement('a');
+  a.setAttribute('href', '_link');
+  a.textContent = link.text;
+  topMenuE1.appendChild(a);
 });
-
-
-
